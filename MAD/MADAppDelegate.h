841fb2566b041eaa70a9016807520586c32a30d1
lai61616 <lai61616@gmail.com>
2012-09-25 11:07:30 +0800

    Initial Commit

---
+++ MAD/MADAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MADAppDelegate.h
+//  MAD
+//
+//  Created by Lai Yi An on 9/25/12.
+//  Copyright (c) 2012 OpenISDM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MADAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MAD/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MAD
+//
+//  Created by Lai Yi An on 9/25/12.
+//  Copyright (c) 2012 OpenISDM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MADAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MADAppDelegate class]));
+    }
+}
